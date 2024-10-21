5d3ae6810b99c75bf0f90a61477785e638c9d2ec
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2024-10-21 11:20:46 -0700

    Update FallDestinationComponent.cs

--- Content.Shared/Gravity/FallDestinationComponent.cs
+++ Content.Shared/Gravity/FallDestinationComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameObjects;
 namespace Content.Shared.GravityFalling
 {
     [RegisterComponent]
-    public sealed class FallDestinationComponent : Component
+    public sealed class FallDestination : Component
     {
         // You can add properties or methods if needed
     }
